def4e854a79d451ebcfad85331abb7067097ab11
Shatirmemer <146601932+Shatirmemer@users.noreply.github.com>
2024-12-08 10:17:31 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=XQMT2aTS#oKSwM-4cI
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '923006838210' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 AUTO_REACT: process.env.AUTO_REACT || "true",
-Antidelete: process.env.Antidelete || "true",
+AntiDelete: process.env.AntiDelete || "true",
 PREFIX: process.env.PREFIX || '.' ,
 FOOTER: process.env.FOOTER=== undefined ? '‌VAJIRA-MD': process.env.FOOTER,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
